8ece73b10926adfd16df562b52859d263febe72d
Sleeponit-Thurs <jasew.erickson@gmail.com>
2023-04-21 11:46:05 -0600

    Created a program that will ask for full name Then display back to user in terminal.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("");
+        Console.Write("What is your Last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {firstName}, {lastName}.");
     }
 }
