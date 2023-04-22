2d8af5773b85f56280c0309e18130167ba3b4313
Sleeponit-Thurs <jasew.erickson@gmail.com>
2023-04-22 12:54:10 -0600

    Completed grade program.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -9,7 +9,7 @@ class Program
         {
             Console.Write("Enter your grade, It will be translated into a letter grade: ");
             gradeInput = Console.ReadLine();
-
+            //Checks for valid input
             if (float.TryParse(gradeInput, out float number))
             {
                 break; // Exit the loop if input is valid
@@ -22,6 +22,7 @@ class Program
 
         float grade = float.Parse(gradeInput);
         float gradeCheck = 60;
+        //List created to print letter grade, using the gradeIndex variable as reference.
         List<string> letterGrade = new List<string> { "F","D","C","B","A"};
         int gradeIndex = 0;
         while (grade >= gradeCheck && gradeCheck != 100)
@@ -31,6 +32,15 @@ class Program
         }
         string assignedGrade = letterGrade[gradeIndex];
         Console.WriteLine($"Here is your assigned grade: {assignedGrade}");
+
+        if (grade < 70) //Check if the user passed, the fail if the grade is not at least 70
+            {
+                Console.WriteLine("Sorry, you did not pass. Try again! You can do it!");
+            }
+        else
+            {
+                Console.WriteLine("Congratulations on passing!");
+            }
 
             
     }
