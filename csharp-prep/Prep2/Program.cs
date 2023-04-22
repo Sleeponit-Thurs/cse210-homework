c573cc6db896c7485a3958a952082661d62bcf41
Sleeponit-Thurs <jasew.erickson@gmail.com>
2023-04-22 12:46:15 -0600

    Grade program, prompts again for invalid input.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeInput;
+        while (true)
+        {
+            Console.Write("Enter your grade, It will be translated into a letter grade: ");
+            gradeInput = Console.ReadLine();
+
+            if (float.TryParse(gradeInput, out float number))
+            {
+                break; // Exit the loop if input is valid
+            }
+         else
+            {
+               Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+
+        float grade = float.Parse(gradeInput);
+        float gradeCheck = 60;
+        List<string> letterGrade = new List<string> { "F","D","C","B","A"};
+        int gradeIndex = 0;
+        while (grade >= gradeCheck && gradeCheck != 100)
+        {
+            gradeCheck = gradeCheck + 10;
+            gradeIndex = gradeIndex + 1;
+        }
+        string assignedGrade = letterGrade[gradeIndex];
+        Console.WriteLine($"Here is your assigned grade: {assignedGrade}");
+
+            
     }
 }
