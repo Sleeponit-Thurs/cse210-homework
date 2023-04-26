adeb84cf312a319494d90824409d0c8892837555
Sleeponit-Thurs <jasew.erickson@gmail.com>
2023-04-26 12:58:16 -0600

    Finished Number game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+        int guess = 0;
+        string guessConvert = "";
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicnumber = randomGenerator.Next(1, 100);
+            Console.Write("Try to guess the magic number!");
+            do
+            {
+               guessConvert = Console.ReadLine();
+               guess = int.Parse(guessConvert);
+               if (guess > magicnumber)
+               {
+                Console.WriteLine("Lower");
+               }
+               else if (guess < magicnumber)
+               {
+                Console.WriteLine("Higher");
+               }
+            } while (guess != magicnumber);
+            Console.Write("Would you like to play again? yes/no");
+            playAgain = Console.ReadLine();
+                if (playAgain == "yes")
+                {
+                    Console.WriteLine("Perfect! Let's go again!");
+                }
+                else if (playAgain== "no")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                }
+        }
+       
     }
 }
